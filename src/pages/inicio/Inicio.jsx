6f690f492d6723d8e85db750ffe09bdbe1e56f5b
Miguel Sosa <80135783+Miguel727@users.noreply.github.com>
2024-07-19 14:20:31 -0300

    Pdf modificado

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -92,17 +92,7 @@ const Inicio = () => {
     }
   };
 
-
-  const generateColumns = (data) => {
-    return Object.keys(data[0])
-      .filter((key, index) => !columnsToSkip.includes(index))
-      .map((key) => ({
-        name: columnNames[key] || key,
-        selector: (row) => row[key],
-      }));
-  };
-
-  //Quitar entrada y salida
+ 
   const generarPDF = () => {
     if (!data) {
       return;
@@ -111,17 +101,15 @@ const Inicio = () => {
     const doc = new jsPDF();
 
     let startX = 10;
-    //let centerX;
     let startY = 20;
 
     const cellHeight = 10;
-    const minRowsPerPage = 20; // Número mínimo de filas por página
+    const minRowsPerPage = 20;
 
     let currentY = startY;
 
     const headers = Object.keys(data[0]);
-    const columnsToSkip = [0, 2, 3, 4, 5, 6, 7, 8, 9,10,13]; // Columnas local
-    //const columnsToSkip = [0, 2, 3, 4, 5, 6, 7, 8, 9, 12]; // Columnas produccion
+    const columnsToSkip = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13];
 
     const columnNames = {
       registro_fecha: "Fecha",
@@ -129,10 +117,8 @@ const Inicio = () => {
       salida: "Salida",
     };
 
-    
-
     const title =
-      "Reporte usuario: " + quitarPrefijoFicha(user.fk_empleado_codigo); // Título del PDF
+      "Reporte usuario: " + quitarPrefijoFicha(user.fk_empleado_codigo);
 
     const monthsInSpanish = [
       "Enero",
@@ -149,27 +135,20 @@ const Inicio = () => {
       "Diciembre",
     ];
 
-    // Obtener el nombre del mes en español
-    const now = new Date(); // Obtener la fecha actual
-    const month = now.getMonth(); // Obtener el valor numérico del mes
-    const spanishMonth = monthsInSpanish[month]; // Obtener el nombre del mes en español
+    const now = new Date();
+    const month = now.getMonth();
+    const spanishMonth = monthsInSpanish[month];
 
-    // Agregar información adicional con el mes en español
-    // const additionalInfo = ` ${spanishMonth} ${now.getFullYear()}`;
-    //const additionalInfo = "Información adicional";
-    // Establecer la alineación de las celdas a 'center'
     doc.setFont("helvetica", "bold");
     doc.setFontSize(10);
     doc.setTextColor(0, 0, 0);
 
-    // Agregar título
     doc.setFontSize(16);
     doc.text(title, startX, currentY);
 
-    // Agregar imagen en el lado opuesto
-    const imageWidth = 75; // Ancho de la imagen
-    const imageHeight = 20; // Alto de la imagen
-    const imageX = doc.internal.pageSize.width - imageWidth - startX; // Posición X de la imagen
+    const imageWidth = 75;
+    const imageHeight = 20;
+    const imageX = doc.internal.pageSize.width - imageWidth - startX;
     doc.addImage(
       logoIntendencia,
       "JPEG",
@@ -179,19 +158,11 @@ const Inicio = () => {
       imageHeight
     );
 
-    currentY += 20; // Espacio después del título
-
-    // Agregar información adicional centrada
-    // const textWidth =
-    //  (doc.getStringUnitWidth(additionalInfo) * doc.internal.getFontSize()) /
-    doc.internal.scaleFactor;
-    // centerX = (doc.internal.pageSize.width - textWidth) / 2;
+    currentY += 20;
 
     doc.setFontSize(14);
-    // doc.text(additionalInfo, centerX, currentY);
-    currentY += 15; // Espacio después de la información adicional
+    currentY += 15;
 
-    // Agregar encabezados de columna con mejor formato (omitir columnas)
     headers.forEach((header, index) => {
       if (!columnsToSkip.includes(index)) {
         const columnName = columnNames[header] || header;
@@ -219,14 +190,12 @@ const Inicio = () => {
 
     let rowsAdded = 0;
 
-    // Agregar filas de datos (omitir columnas)
     data.forEach((row) => {
       if (rowsAdded >= minRowsPerPage) {
         doc.addPage();
         currentY = startY;
         rowsAdded = 0;
 
-        // Agregar encabezados de columna nuevamente en la nueva página
         headers.forEach((header, index) => {
           if (!columnsToSkip.includes(index)) {
             const columnName = columnNames[header] || header;
@@ -265,7 +234,6 @@ const Inicio = () => {
               columnsToSkip.filter((col) => col < columnIndex).length) *
               columnWidth;
           doc.rect(cellX, currentY, columnWidth, cellHeight, "S");
-          // Formatear la fecha antes de agregarla al PDF (inversión)
           if (headers[columnIndex] === "registro_fecha") {
             const dateParts = value.split("-");
             const formattedDate = `${dateParts[2]}/${dateParts[1]}/${dateParts[0]}`;
@@ -293,9 +261,9 @@ const Inicio = () => {
       rowsAdded += 1;
     });
 
-    // Guardar el PDF como archivo descargable
     doc.save("Reporte_" + quitarPrefijoFicha(user.fk_empleado_codigo) + ".pdf");
   };
+
 
   const columnas = [
     {
@@ -369,13 +337,13 @@ const Inicio = () => {
             </Col>
           </Row>
 
-       {/*   <Button
+          <Button
             variant="primary"
             onClick={generarPDF}
             className="custom-button"
           >
             Descargar PDF
-           </Button>  */}
+           </Button>  
         </Form>
 
         {data && (
